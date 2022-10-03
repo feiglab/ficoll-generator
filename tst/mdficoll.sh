9b3c75feee32e363aaf62796eb82fc30daa44111
Michael Feig <mfeiglab@gmail.com>
2022-10-03 12:34:21 -0400

    updates for di/triglycerol

--- tst/mdficoll.sh
+++ tst/mdficoll.sh
@@ -5,6 +5,6 @@ patchfile=$2
 
 patchlist=`cat $patchfile | xargs | tr ' ' '_'`
 
-minCHARMM.pl -log min.log -par param=x,xtop=top_all36_carb.rtf:patches-linkers-all.rtf,xpar=par_all36_carb.prm:patches-linkers-xtra.prm,patch=$patchlist,minsteps=100 $pdbfile > min.pdb
-mdCHARMM.pl -log md.log -par param=x,xtop=top_all36_carb.rtf:patches-linkers-all.rtf,xpar=par_all36_carb.prm:patches-linkers-xtra.prm,patch=$patchlist,dielec=rdie,epsilon=4,dyntemp=300,dynsteps=10000,dynoutfrq=500 -final md.pdb -trajout md.dcd min.pdb
+minCHARMM.pl -log min.log -par param=x,xtop=top_all36_carb.rtf:poly-glycerol.rtf:patches-linkers-all.rtf,xpar=par_all36_carb.prm:patches-linkers-xtra.prm,patch=$patchlist,nobuildall,minsteps=100 $pdbfile > min.pdb
+mdCHARMM.pl -log md.log -par param=x,xtop=top_all36_carb.rtf:poly-glycerol.rtf:patches-linkers-all.rtf,xpar=par_all36_carb.prm:patches-linkers-xtra.prm,patch=$patchlist,nobuildall,dielec=rdie,epsilon=4,dyntemp=300,dynsteps=10000,dynoutfrq=500 -final md.pdb -trajout md.dcd min.pdb
 
